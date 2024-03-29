a0b8d1a7474dd4002fcb00b99c229a178d37e98a
martell <sergiy.listvagov@sdkfinance.com>
2024-03-29 13:22:43 +0200

    add submodule

--- src/load-external-modules.js
+++ src/load-external-modules.js
@@ -1,8 +1,12 @@
 let EXTERNAL_MODULES = [];
 
 if(import.meta.env.VITE_WITH_SUBMODULES == 'true'){
-  const EXTERNAL = await import('../../modules/index');
-  EXTERNAL_MODULES = Object.values(EXTERNAL.default) ?? []
+  try {
+    const EXTERNAL = await import('../feature/modules/index');
+    EXTERNAL_MODULES = Object.values(EXTERNAL.default) ?? []
+  } catch (error) {
+    console.log('Load external modules ERROR:', { error });
+  }
 }
 
 console.log({ EXTERNAL_MODULES });
